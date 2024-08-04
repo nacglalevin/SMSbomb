8d01ebef34a7c054f2125c68c0e277a667a879f7
Lalevin <zzlyxht@outlook.com>
2024-08-04 10:23:49 +0800

    更新 threadpool.py

--- threadpool.py
+++ threadpool.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
+# beloved/DHS
 #-*-coding:utf-8-*-
-
-# ThreadPool is a simple thread pool
-#
-# Copyright (C) 2012 Yummy Bian <yummy.bian#gmail.com>
-#
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ThreadPool is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.";
-#
-
 import sys
 import Queue
 import threading
